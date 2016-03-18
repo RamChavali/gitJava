2f4c9b339329f577021ca338e95796add44c8e6b
Grace <gracernellore@gmail.com>
2016-03-17 22:54:14 -0700

    adding GoKart

---
+++ java_Basics/GoKart/GoKart.java
@@ -0,0 +1,38 @@
+public class GoKart {
+  public static final int MAX_BARS = 8;
+  private String mColor;
+  private int mBarsCount;
+  
+  public GoKart(String color) {
+    mColor = color;
+    mBarsCount = 0;
+  }
+  
+  public String getColor() {
+    return mColor;
+  }
+
+  public void drive() {
+    drive(1);
+  }
+
+  public void drive(int laps) {
+    // Other driving code omitted for clarity purposes
+    mBarsCount -= laps;
+  }
+  
+  public void charge() {
+    while (!isFullyCharged()) {
+      mBarsCount++;
+    }
+  }
+  
+  public boolean isBatteryEmpty() {
+    return mBarsCount == 0;
+  }
+
+  public boolean isFullyCharged() {
+    return mBarsCount == MAX_BARS;
+  }
+
+}
