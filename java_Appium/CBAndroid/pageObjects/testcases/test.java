8f8e1c4635b80a5505386e89c90f5671e253a4b1
Grace <gracernellore@gmail.com>
2016-04-05 10:23:10 -0700

    adding todos

--- java_Appium/CBAndroid/pageObjects/testcases/test.java
+++ java_Appium/CBAndroid/pageObjects/testcases/test.java
@@ -1,1 +1,63 @@
-//add test
+// ###########################################
+//			-- add tests --
+// ###########################################
+
+/* addToCart tests
+ * ----------------------------------
+ * addToCart_fromStorePage
+ * addToCart_fromProductDetailPage
+ * addToCart_fromSearchStoreLevel
+ * addToCart_fromSearchStoreCategoryLevel
+ * addToCart_fromSearchMallLevel
+ * addToCart_fromSearchMallCategoryLevel
+ */
+
+/* changeQty tests
+ * -------------------------------
+ * changeQty_fromStorePage
+ * changeQty_fromProductDetailPage
+ * changeQty_fromCartPage
+ */
+
+/* removeQty tests
+ * --------------------------------
+ * removeQty_fromStorePage
+ * removeQty_fromProductDetailPage
+ * removeQty_fromCartPage
+ */
+
+/* orderDetailsUI tests
+ * -------------------------------
+ * orderDetailsUI_cancelled
+ * orderDetailsUI_collected
+ * orderDetailsUI_inProgress
+ * orderDetailsUI_curbsideReady
+ * orderDetailsUI_pickupReady
+ * orderDetailsUI_needsReview_*
+ */
+
+// ###########################################
+// 			-- api-related --
+// ###########################################
+
+/* login tests
+ * validLogin_afterLogoutSameAcct
+ * validLogin_afterLogoutDiffAcct
+ */
+
+/* loyaltyCard tests
+ * loyaltyCard_addNew
+ * loyaltyCard_remove
+ */
+
+
+/* storeCategories tests
+ * storeCategories_allOption
+ */
+
+
+
+
+
+
+
