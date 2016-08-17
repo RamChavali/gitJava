c26981237fd6543ca98dbb5058c650f2a58318b8
Grace <gracernellore@gmail.com>
2016-08-16 19:11:44 -0700

    refactored string reversal per Yun, 2 line O.M.G. https://github.com/havefuncoding/gitProjects/issues/12

--- java_Yun/java_Yun/LeetcodeReverseString.java
+++ java_Yun/java_Yun/LeetcodeReverseString.java
@@ -1,5 +1,15 @@
 package java_Yun;
 
+/**
+ * Additional resources:
+ * 1) Yun!
+ * 2) StringBuilder to String, http://stackoverflow.com/a/16155815
+ * 3) reverse string using String Builder, http://www.tutorialspoint.com/java/lang/stringbuilder_reverse.htm
+ * 
+ * 
+ *
+ */
+
 public class LeetcodeReverseString {
 	
     public static String reverseString(String originalString) {
@@ -18,6 +28,29 @@ public class LeetcodeReverseString {
 		System.out.println("reversedString:\t" + reversedString);
     	return reversedString;
     }
+    
+    
+    // refactored version using StringBuilder!
+    public static String reverseStringUsingStringBuilder(String originalString) {
+    	String reversedString = new StringBuilder(originalString).reverse().toString();
+    	System.out.println("reverseStringUsingStringBuilder:\t" + reversedString);
+    	return reversedString;
+    }
+    
+    // refactored version using StringBuffer!
+    public static String reverseStringUsingStringBuffer(String originalString) {
+    	String reversedString = new StringBuffer(originalString).reverse().toString();
+    	System.out.println("reverseStringUsingStringBuffer:\t" + reversedString);
+    	return reversedString;
+    }
+    
+    
+	public static void main(String[] args) { 
+		
+		// TEST
+		reverseString("hello"); 
+		reverseStringUsingStringBuilder("hello2");
+		reverseStringUsingStringBuffer("hello3");
+	}
 	
-	public static void main(String[] args) { reverseString("hello"); }
 }
